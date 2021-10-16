73750b90bc72a532713c9dd64aa30e2796cb326c
ico2k2developer <48391330+ico2k2developer@users.noreply.github.com>
2021-10-16 14:07:26 +0200

    Update Esercizio4.py

--- Esercizio4.py
+++ Esercizio4.py
@@ -1,3 +1,19 @@
+'''
+Lo pseudocodice seguente descrive come, in una libreria, viene calcolato l’importo di 
+un ordine a partire dal costo totale dei libri ordinati e dal loro numero.  
+ 
+• Leggere il costo totale dei libri e il numero di libri. 
+• Calcolare le tasse (il 7.5 per cento del costo totale dei libri). 
+• Calcolare i costi di spedizione ($2 per ogni libro). 
+• Il prezzo totale dell’ordine è la somma del costo totale dei libri, delle tasse e 
+dei costi di spedizione. 
+• Visualizzare l’importo dell’ordine. 
+ 
+Scrivere un programma in Python che implementi questo pseudocodice. Il costo 
+totale dei libri e il numero di libri devono essere memorizzati in due variabili 
+costanti.
+'''
+
 count = int(input("Inserisci il numero di libri: "))
 total = float(input("Inserisci il costo totale dei libri: "))
 
