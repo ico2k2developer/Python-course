c0f1e7cd7f05fd69a59461abcb5bcc6e6ddeb63f
ico2k2developer <48391330+ico2k2developer@users.noreply.github.com>
2021-10-16 14:04:52 +0200

    Update Esercizio2.py

--- Esercizio2.py
+++ Esercizio2.py
@@ -1,3 +1,11 @@
+'''
+Scrivere un programma che memorizzi in una costante un numero intero positivo di 
+cinque cifre (quindi compreso tra 10000 e 99999), e visualizzi le singole cifre di cui 
+è composto.  
+Ad esempio, avendo il numero 16384, il programma deve visualizzare, su righe 
+separate: 1 6 3 8 4.
+'''
+
 CONSTANT = input("Inserisci un numero intero positivo: ")
 
 index = 0
