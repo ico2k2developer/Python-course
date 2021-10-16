d2874f314a0b266253473340db6c870a3b2a2567
ico2k2developer <48391330+ico2k2developer@users.noreply.github.com>
2021-10-16 14:03:40 +0200

    Update Esercizio1.py

--- Esercizio1.py
+++ Esercizio1.py
@@ -1,3 +1,19 @@
+'''
+Scrivere un programma che memorizzi due numeri interi in due costanti definite nel 
+codice, e poi ne visualizzi: 
+• La somma 
+• La differenza 
+• Il prodotto 
+• Il valore medio 
+• La distanza (cioè il valore assoluto della differenza) 
+• Il valore massimo (cioè il maggiore tra i due) 
+• Il valore minimo (cioè il minore tra i due) 
+ 
+Suggerimento: utilizzare le funzioni max e min definite in Python. Esse accettano 
+una sequenza di valori separati da virgola in input e restituiscono rispettivamente il 
+valore massimo e minimo della sequenza. (Es: max(10, 5) restituisce 10).
+'''
+
 import math
 
 CONSTANT1 = math.e
