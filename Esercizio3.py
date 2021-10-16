2c7a339966fd2c536829d0c3ad74d5c5dbe5f186
ico2k2developer <48391330+ico2k2developer@users.noreply.github.com>
2021-10-16 14:06:06 +0200

    Update Esercizio3.py

--- Esercizio3.py
+++ Esercizio3.py
@@ -1,3 +1,12 @@
+'''
+Scrivere un programma che memorizzi una stringa in una variabile e, a partire da 
+quella variabile, visualizzi i primi tre caratteri della stringa, seguiti da tre punti, 
+ancora seguiti dagli ultimi tre caratteri. 
+Ad esempio, se la stringa viene inizializzata al valore “Mississippi”, il programma 
+deve visualizzare “Mis...ppi”. [P2.22] 
+Cosa succede della stringa è più corta di 6 caratteri? E se è più breve di 3 caratteri?
+'''
+
 string = input("Inserisci una stringa: ")
 
 if len(string) >= 6:
